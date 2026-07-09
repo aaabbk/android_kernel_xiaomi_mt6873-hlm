d7022b1a436b896c9582021d072da4bee5b4a14d
aaabbk <2268951925@qq.com>
2026-07-09 11:25:54 +0800

    ion: debug: add detailed logging for ion_alloc failure

--- drivers/staging/android/ion/ion-ioctl.c
+++ drivers/staging/android/ion/ion-ioctl.c
@@ -4,7 +4,7 @@
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
+ * may be copied, distributed, and modified under that license.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -111,6 +111,11 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			IONMSG(
 				"ION_IOC_ALLOC handle is invalid. ret = %d.\n",
 				ret);
+			pr_err("[ION_DEBUG] ION_IOC_ALLOC from pid %d (%s): len=%u, align=%u, heap_mask=0x%x, flags=0x%x, ret=%ld\n",
+			       current->pid, current->comm,
+			       data.allocation.len, data.allocation.align,
+			       data.allocation.heap_id_mask, data.allocation.flags,
+			       PTR_ERR(handle));
 			return PTR_ERR(handle);
 		}
 
